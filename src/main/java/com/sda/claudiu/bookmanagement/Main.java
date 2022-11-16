7bf1641de5819a64f8b8166ccb840e0e2099b33b
Claudiu Tat <clawtat@gmail.com>
2022-11-16 19:48:09 +0200

    BMS-16: Add Hibernate model.

--- src/main/java/com/sda/claudiu/bookmanagement/Main.java
+++ src/main/java/com/sda/claudiu/bookmanagement/Main.java
@@ -1,7 +1,10 @@
 package com.sda.claudiu.bookmanagement;
 
+import com.sda.claudiu.bookmanagement.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
 
     }
 }
--- src/main/java/com/sda/claudiu/bookmanagement/utils/SessionManager.java
+++ src/main/java/com/sda/claudiu/bookmanagement/utils/SessionManager.java
@@ -1,6 +1,9 @@
 package com.sda.claudiu.bookmanagement.utils;
 
 
+import com.sda.claudiu.bookmanagement.model.Author;
+import com.sda.claudiu.bookmanagement.model.Book;
+import com.sda.claudiu.bookmanagement.model.BookReview;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -22,6 +25,9 @@ public class SessionManager extends AbstractSessionManager {
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
         // Hibernate model will be added here
+        configuration.addAnnotatedClass(Author.class);
+        configuration.addAnnotatedClass(Book.class);
+        configuration.addAnnotatedClass(BookReview.class);
     }
 
 }
